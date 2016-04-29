ef9db92b90e6858f2829e9c00e16b670f4888d0e
PotatoJP <2012042063@ust-ics.mygbiz.com>
2016-04-29 12:46:08 +0800

    feat: add button to banner and add list items to campus

--- template-campus.php
+++ template-campus.php
@@ -51,6 +51,29 @@
             endwhile; // End of the loop.
           ?>
 
+          <div class="section cs-list-items">
+            <div class="col-md-3 cs-image">
+              <?php
+                while ( have_posts() ) : the_post();
+
+                if(has_post_thumbnail()){ $featured_img = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); } else { $featured_img = "http://lorempixel.com/300/250"; }
+
+                endwhile; // End of the loop.
+              ?>
+              <img src="<?php echo $featured_img; ?>">    
+            </div>
+            <div class="col-md-9 cs-items">
+              <h4>Faculty of Pharmacy Student Council (FPSC)</h4>
+              <h6>FPSC is the student government in the Faculty of Pharmacy (including Medical Technology and Biochemistry). The
+              Council shall serve as official liason between the studentry and the faculty and university officials. It may be involved
+              or may take charge of activities that will lead to the intellectual, spiritual, physical and social upliftment of the students
+              in coordination with the other recognized organizations of the Faculty.</h6>
+              <div class="list-details">
+                <i class="fa fa-circle fa.symbol"></i><h5 class="mini-head">Sample H5: </h5><h6 class="mini-details"><i>Sample H6</i></h6>
+              </div>
+            </div>
+          </div>
+
         </div>
         
       </div>
